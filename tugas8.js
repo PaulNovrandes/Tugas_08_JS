adfe1540d22c8d93071ec0d28eda4cab38ee8259
PaulNovrandes <novrandes.tanuma@gmail.com>
2023-04-25 16:01:41 +0700

    Tugas 08 JS
    Menambah dan mengubah property. tugas ini dikumpulkan kepada niomic

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+function ambilData(){
+  var arloji = {
+    type: "HA-105",
+    warna: "Coklat tua",
+    diameter: 48
+  }
+
+  arloji.warna = "Black";
+  arloji.strap = "kulit";
+  console.log(arloji);
+  console.log(arloji.warna);
+}
+
+ambilData();
